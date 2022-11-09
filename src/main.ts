549ba4df8d5ef234113210ecfb884efbbe9b731e
Sumit <sj.sumitjangir@gmail.com>
2022-11-09 11:04:16 +0530

    define attributes and implement there corresponding functionality

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import './style.css'
 import './basic-editor';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <basic-editor></basic-editor>
+  <basic-editor 
+    tabindex="0" 
+    focus="true" 
+    initial-message="Kindly, write here..." 
+    tab-force-focus="false" 
+    focusout-force-focus="false"
+    disable-next-line="false"
+    disabled="false">
+  </basic-editor>
 `;
